a44dc833713fd8bec29ba6e6a618bc6da045eac6
Dmytro <d.salyakin@gmail.com>
2025-01-19 04:35:44 +0200

    Add and style new elements in image gallery card

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,11 +2,31 @@ export const createdGalleryCardTemplate = imgInfo => {
   return `
     <li class="gallery-item">
       <a class="gallery-link" href="${imgInfo.largeImageURL}">
-        <img
-          class="gallery-image"
-          src="${imgInfo.webformatURL}"
-          alt=""
-        />
+        <figure class="gallery-figure">
+          <img
+            class="gallery-image"
+            src="${imgInfo.webformatURL}"
+            alt="${imgInfo.tags}"
+          />
+          <figcaption class="image-stats">
+            <div class="stat-item">
+              <p class="stat-label">Likes</p>
+              <p class="stat-value">${imgInfo.likes}</p>
+            </div>
+            <div class="stat-item">
+              <p class="stat-label">Views</p>
+              <p class="stat-value">${imgInfo.views}</p>
+            </div>
+            <div class="stat-item">
+              <p class="stat-label">Comments</p>
+              <p class="stat-value">${imgInfo.comments}</p>
+            </div>
+            <div class="stat-item">
+              <p class="stat-label">Downloads</p>
+              <p class="stat-value">${imgInfo.downloads}</p>
+            </div>
+          </figcaption>
+        </figure>
       </a>
     </li>
   `;
